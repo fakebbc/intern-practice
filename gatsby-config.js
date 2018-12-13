269c2d16b13a53b54d8d9e8979dbc0911c5c55d7
Lachlan Stibbard Hawkes <lachlan.s-hawkes@xero.com>
2018-12-13 14:50:13 +1100

    update project prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
     author: '@gatsbyjs',
   },
-  pathPrefix: "/fake-bbc",
+  pathPrefix: "/intern-practice",
   plugins: [
     'gatsby-plugin-react-helmet',
     {
